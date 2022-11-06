b68adc24dfe3d026316a4da00d70f506b40e59f3
harumarix <khalluf.amal@gmail.com>
2022-11-07 01:47:02 +0300

    test2

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -9,7 +9,6 @@ const Profile = () => {
   const logoutHandler = () => {
     dispatch(authActions.logout());
     navigate("/login");
-    console.log(1);
   };
   return (
     <section className={`sectionContent ${classes.profile}`}>
